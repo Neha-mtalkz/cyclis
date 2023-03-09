4d9a420cd8fc95eddb63b66f34ea56c98b0eac0e
Neha-mtalkz <109784764+Neha-mtalkz@users.noreply.github.com>
2023-03-09 16:16:23 +0530

    Update index.js
    aa

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 
 console.log("*******************************")
+app.post("/karix-callback", (req, res) => {
+    console.log(req.body, "******************")
+    res.send(res.body)
+})
+
+console.log("*******************************")
 
 app.listen(7000, (err) => {
     if(err) console.log(err)
