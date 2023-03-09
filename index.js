2e96727bb1d2548518c843ff4a9bf13e006a4728
Neha-mtalkz <109784764+Neha-mtalkz@users.noreply.github.com>
2023-03-09 16:35:17 +0530

    Update index.js
    app.use(express.json())

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
 console.log("*******************************")
 app.post("/karix-callback", (req, res) => {
     console.log(req.data, req.body, req, "******************")
